a98fb7df27e4c77ff5b48ad0d03bedaa3c1a228b
yeseob <jakalob103@naver.com>
2024-03-01 15:42:44 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -9,4 +9,11 @@ function login() {
     id: id.value,
     password: password.value,
   };
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
 }
