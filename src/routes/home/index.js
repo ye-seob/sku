c3871600d25669c95e9fcdbc32bf0d6beba7287a
ye-seob <161628841+ye-seob@users.noreply.github.com>
2024-03-01 10:52:41 +0900

    Delete src directory

--- src/routes/home/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const ctrl = require("./home.ctrl");
-
-router.get("/", ctrl.home);
-router.get("/login", ctrl.login);
-
-module.exports = router;
