432dbebd2ffbf723ba4148176d9bcfecd662a7eb
yeseob <jakalob103@naver.com>
2024-03-01 10:51:29 +0900

    폴더구조 최적화

---
+++ app/app.js
@@ -0,0 +1,16 @@
+//모듈
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+//라우팅
+const home = require("./src/routes/home");
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home); //use -> 미들웨어를 등록해주는 메서드
+
+module.exports = app;
